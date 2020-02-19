20213d2945dc3a053baadc7b165bf26793d443e3
Philip Evans <42783187+pevans172@users.noreply.github.com>
2020-02-19 18:04:09 +0000

    update to prac 3

--- Prac_3/Collections/BankAccount.java
+++ Prac_3/Collections/BankAccount.java
@@ -4,6 +4,7 @@ public class BankAccount
     private int uniqueAccountId;
     private Person owner;
     private int balance;
+    private Transaction t;
     private ArrayList<Transaction> transactions;
 
     public BankAccount(int uniqueAccountId, Person owner, int balance)
@@ -22,10 +23,10 @@ public class BankAccount
     {
         this.balance = balance;
     }
-    public void addTransaction(int amountInvolved, BankAccount sender, BankAccount reciever)
+    public void addTransaction(int amountInvolved, BankAccount reciever)
     {
-        Transaction temp = new Transaction(amountInvolved, sender, reciever);
-        transactions.add(temp);
+        t = new Transaction(amountInvolved, this, reciever);
+        transactions.add(t);
     }
     
     public int getUniqueAccountId()
@@ -43,5 +44,16 @@ public class BankAccount
     public ArrayList<Transaction> getTransactions()
     {
         return transactions;
+    }
+    
+    public void printTransactions()
+    {
+        for (int i=0; i < transactions.size(); i++)
+        {
+            System.out.println("Transaction " + i);
+            System.out.println("Amount: " + transactions.get(i).getAmountInvolved());
+            System.out.println("Sent to: " + transactions.get(i).getReciever());
+            System.out.println("Sent by: " + transactions.get(i).getSender());
+        }
     }
 }
